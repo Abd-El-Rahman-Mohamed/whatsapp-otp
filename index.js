07fc81b3e57b5f706e432a9f85cc136f6fb015ed
Abd-El-Rahman-Mohamed <webdevelopmentlj@gmail.com>
2025-05-18 19:49:45 +0300

    Bug fixes

--- index.js
+++ index.js
@@ -12,7 +12,17 @@ app.use(express.urlencoded({ extended: true }));
 const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process',
+      '--disable-gpu'
+    ],
+    headless: true,
   }
 });
 
